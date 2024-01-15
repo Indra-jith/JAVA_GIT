6b11b69952b0e762b8e4e5b17b073adb9e4fbb3b
indra-jith <indrajithmp77@gmail.com>
2024-01-15 20:38:13 +0530

    data types

--- INTRODUCTION/Variables.java
+++ INTRODUCTION/Variables.java
@@ -1,3 +1,18 @@
+/*
+ * DATATYPES - 
+ * NON primitive -> Arrays,class..
+ 
+ * Primitive -> Bool (true/false)
+             -> Char (single character 'a' single quote)
+             -> Integers (int,long,short,byte) 
+                           4   8     2    1   <- size in bytes
+             note :  byte only uses 1 byte and can represent -128 to 127
+             -> Floating points (float,double)
+                                   4     8   <- size in bytes
+
+
+ */
+
 public class Variables {
     public static void main(String[] args) {
         int num = 5;
