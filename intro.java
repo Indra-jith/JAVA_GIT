abaf09c99fb7e142576636a4a59259294f874de7
indra-jith <indrajithmp77@gmail.com>
2024-01-15 19:58:03 +0530

    intro

--- intro.java
+++ intro.java
@@ -1,6 +1,15 @@
 /*
- * JVM (JAVA VIRTUAL MACHINE)
- *
+ * JVM (Java Virtual Machine)
+IT provides a run time environment in which java bytecode can be executed
+It can also run those programs which are written in other languages and compiled to Java
+Bytecode.
+
+ *JRE (Java Runtime Environment)
+ It is a set of software tools and libraries + other files which
+ are used for developing and it provides runtime environment
+
+ *JDK (Java Development Kit)
+ It helps us to develop Java applications, it contains JRE + development tools.
  */
 public class intro{
     public static void main(String[] args) {
