4b5ee0803e28eeac8b1ff3f5107880df341217ac
indra-jith <indrajithmp77@gmail.com>
2024-01-16 20:50:17 +0530

    typecasting

--- INTRODUCTION/Variables.java
+++ INTRODUCTION/Variables.java
@@ -1,6 +1,6 @@
 /*
  * DATATYPES - 
- * NON primitive -> Arrays,class..
+ * Non primitive -> Arrays,class..
  
  * Primitive -> boolean (true/false)
              -> Char (single character 'a' single quote)
@@ -15,16 +15,35 @@
 
 public class Variables {
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 10;
-        int result = num1+num2;
-        System.out.println(result); //15
+        // int num1 = 5;
+        // int num2 = 10;
+        // int result = num1+num2;
+        // System.out.println(result); //15
 
-        float num3 = 10.5f; //without f -> treats as double by default
-        byte num4 = 11;
-        double num5 = 11.111111;
-        long num6 = 1321;
-        boolean val = true;
-        System.out.println(val);
+        // float num3 = 10.5f; //without f -> treats as double by default
+        // byte num4 = 11;
+        // double num5 = 11.111111;
+        // long num6 = 1321;
+        // boolean val = true;
+        // System.out.println(val);
+
+         /*
+         TYPECASTING -> two types
+                One happens automatically (smaller to larger) [byte->short->int->long]
+                *smaller box fits in larger box automatically -> WIDENING CASTING
+                One happens manually (example: long->int->short->byte)
+                NARROWING CASTING
+        */
+
+        // int myInt = 10;
+        // double myDouble = myInt; //automatic casting
+        // System.out.println(myDouble); //10.0 --No problemo!
+
+        double myDouble = 3.14567;
+        // int myInt = myDouble; //error (we have to do it forcefully)
+        int myInt = (int) myDouble; //--> FORCED/MANUAL/NARROWING
+        System.out.println(myInt); //3
+
     }
 }
+
