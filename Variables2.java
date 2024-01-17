1f627333ad293ba212da0318d96d5fc33fc4746f
indra-jith <indrajithmp77@gmail.com>
2024-01-17 19:01:34 +0530

    basic operations

---
+++ Variables2.java
@@ -0,0 +1,9 @@
+public class Variables2 {
+    public static void main(String args[])
+    {
+        int num = 10;
+        float num2 = 1.55f;
+        System.out.println(num+num2); //11.55
+        System.out.println(num*num2); //15.5
+    }
+}
