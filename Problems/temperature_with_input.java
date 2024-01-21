0ced4a7f35c230dff0c97046ae6e711dc67b5ac6
indra-jith <indrajithmp77@gmail.com>
2024-01-21 17:30:28 +0530

    taking inputs from user

---
+++ Problems/temperature_with_input.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class temperature_with_input {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your temperature in degree celsius: ");
+        float degree = scanner.nextFloat();
+        float farheh = (degree*9/5)+32;
+        System.out.println(degree+" degrees is equal to "+farheh+" farhenheit");
+    }
+}
