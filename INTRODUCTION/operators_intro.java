3e7a93552b63beb62f77876faaa7245e31a688dd
indra-jith <indrajithmp77@gmail.com>
2024-01-22 19:21:05 +0530

    operators

---
+++ INTRODUCTION/operators_intro.java
@@ -0,0 +1,14 @@
+// + - * / %
+//create two variables and perform above mentioned operations
+//and print the result
+public class operators_intro {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+        System.out.println(num1/num2);
+        System.out.println(num1%num2);
+    }
+}
