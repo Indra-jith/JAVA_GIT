bd83e7737b2f51548dd9c41f81263d4bb9f4bf09
indra-jith <indrajithmp77@gmail.com>
2024-01-23 20:45:09 +0530

    loops

---
+++ Problems/loop_question.java
@@ -0,0 +1,9 @@
+public class loop_question {
+    public static void main(String[] args) {
+        // print the table of 13 using loops
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println("13 X "+i+" = "+(13*i));
+        }
+    }
+}
