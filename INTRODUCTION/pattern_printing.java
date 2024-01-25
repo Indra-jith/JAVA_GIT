c42aab04a7602da932cab93d39ab4f08fef6fd57
indra-jith <indrajithmp77@gmail.com>
2024-01-25 23:49:52 +0530

    new pattern

--- INTRODUCTION/pattern_printing.java
+++ INTRODUCTION/pattern_printing.java
@@ -95,5 +95,25 @@ public class pattern_printing {
             }
             System.out.print("\n");
         }
+
+        /*
+         * 
+         * * * * *
+         * * * *
+         * * *
+         * *
+         *
+         */
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 5 - i; k++) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+
     }
 }
