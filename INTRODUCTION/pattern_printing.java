b7ed5116ebc2751dd6388879f317f9e8cc2accf0
indra-jith <indrajithmp77@gmail.com>
2024-01-27 13:58:22 +0530

    pyramid pattern

--- INTRODUCTION/pattern_printing.java
+++ INTRODUCTION/pattern_printing.java
@@ -109,6 +109,19 @@ public class pattern_printing {
             }
             System.out.print("\n");
         }
+        System.out.print("\n");
 
+        /*
+         * (pyramid)
+         */
+        for (int i = 5; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 5 - i; k++) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
     }
 }
