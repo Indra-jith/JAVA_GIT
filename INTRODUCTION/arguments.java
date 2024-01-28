a3fea70844d536fd49b7fc99117d2c0c04c51ba1
indra-jith <indrajithmp77@gmail.com>
2024-01-28 18:40:44 +0530

    arguments

---
+++ INTRODUCTION/arguments.java
@@ -0,0 +1,14 @@
+public class arguments {
+    public static void main(String[] args) {
+        // arguments are given along with commandline and first one is args[0] second
+        // args[1]
+        // and so on
+        System.out.println("The first argument is : " + args[0]);
+        System.out.println("The second argument is : " + args[1]);
+        /*
+         * java arguments hello hi
+         * The first argument is : hello
+         * The second argument is : hi
+         */
+    }
+}
