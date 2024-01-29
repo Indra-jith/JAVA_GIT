4fd93cc5cec6fa10c7739a8c7971bf10e3783093
indra-jith <indrajithmp77@gmail.com>
2024-01-29 19:17:06 +0530

    loops

--- Problems/input.java
+++ Problems/input.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class input {
     public static void main(String args[]) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a string(your name): ");
-        String name = scan.nextLine();
+        String name = scanner.nextLine();
         System.out.println("Your name is " + name);
     }
 }
