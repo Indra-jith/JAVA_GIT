804a354dea3de975f54aae9b2aa2e754b6955b4b
indra-jith <indrajithmp77@gmail.com>
2024-01-31 19:08:41 +0530

    additional problems

--- INTRODUCTION/Arrays/multidimensional_arrays.java
+++ INTRODUCTION/Arrays/multidimensional_arrays.java
@@ -6,9 +6,9 @@ public class multidimensional_arrays {
         // 3rd row 1st column, index-> arr[2][0]
 
         int arr2[][] = {
-                { 1, 2, 3 }, // first row
+                { -1, 2, 3 }, // first row
                 { 4, 5, 6 }, // second row
-                { 7, 8, 9 } // third row
+                { 7, -8, 9 } // third row
         };
 
         // for (int i = 0; i < arr2.length; i++) {
@@ -48,7 +48,7 @@ public class multidimensional_arrays {
         // 4 . 6
         // 7 8 9
 
-        // PRINT ALL ELEMENTS BELOW DIAGONAL
+        // PRINT ALL ELEMENTS BELOW DIAGONAL (includind diagonal)
         for (int i = 0; i < arr2.length; i++) {
             for (int j = 0; j < arr2[0].length; j++) {
                 if (i < j)
@@ -59,14 +59,50 @@ public class multidimensional_arrays {
             }
             System.out.println();
         }
+        System.out.println();
         // output
         // 1
         // 4 5
         // 7 8 9
 
         // swap first row with last row of array
+
         // replace all the negative elements of array with 0
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                if (arr2[i][j] < 0) {
+                    arr2[i][j] = 0;
+                }
+            }
+        }
+
         // find max element in array and print
+        int max = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                if (arr2[i][j] > max) {
+                    max = arr2[i][j];
+                }
+            }
+        }
+        System.out.println("Max element: " + max);
+
         // sum of each row in an array
+        int sumrow = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                sumrow += arr2[i][j];
+            }
+            System.out.println("Sum of row " + (i + 1) + "= " + sumrow);
+            sumrow = 0;
+        }
+
+        // printing
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr2[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
