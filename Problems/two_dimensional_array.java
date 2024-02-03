14b93cadff4413938e8b5ba9cb5b14690796b895
indra-jith <indrajithmp77@gmail.com>
2024-02-03 13:08:39 +0530

    sum of each column

--- Problems/two_dimensional_array.java
+++ Problems/two_dimensional_array.java
@@ -1,29 +1,40 @@
 public class two_dimensional_array {
     public static void main(String[] args) {
-        //replace first and last row 
         int arr[][] = {
             { -1, 2, 3}, // first row
             { 4, 5, 6 }, // second row
-            { 7, -8, 9},// third row
-            {10, 4, 4} //fourth row
+            { 7, -8, 9}// third row
             };
 
         int numRows = arr.length;
         int numCols = arr[0].length;
 
+        // //replace first and last row 
+        // for(int i=0;i<numCols;i++)
+        // {
+        //     int temp = arr[0][i];
+        //     arr[0][i] = arr[numRows-1][i];
+        //     arr[numRows-1][i] = temp;
+        // }
+        // for(int i=0;i<arr.length;i++)
+        // {
+        //     for(int j=0;j<arr[0].length;j++)
+        //     {
+        //         System.out.print(arr[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+        //sum of each columns
+        int sum=0;
         for(int i=0;i<numCols;i++)
         {
-            int temp = arr[0][i];
-            arr[0][i] = arr[numRows-1][i];
-            arr[numRows-1][i] = temp;
-        }
-        for(int i=0;i<arr.length;i++)
-        {
-            for(int j=0;j<arr[0].length;j++)
+            for(int j=0;j<numRows;j++)
             {
-                System.out.print(arr[i][j]+" ");
+                sum+=arr[j][i];
             }
-            System.out.println();
+            System.out.println("Sum of "+ i +" column = "+sum);
+            sum=0;
         }
     }
 }
