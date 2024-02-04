3d0d876b0de741c79b165ed2fb393007bc13b605
indra-jith <indrajithmp77@gmail.com>
2024-02-04 23:06:20 +0530

    string to other data type conversion

---
+++ Problems/parse.java
@@ -0,0 +1,8 @@
+public class parse {
+    public static void main(String[] args) {
+        String num = "12345";
+        int num1 = Integer.parseInt(num);
+        System.out.println(num1);
+        // Similarly Float.parseFloat, Double.parseDouble..etc
+    }
+}
