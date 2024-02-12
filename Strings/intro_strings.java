6d6bb1a3d83e0ac518f7722991c93b4494616c5c
indra-jith <indrajithmp77@gmail.com>
2024-02-12 20:56:47 +0530

    Strings intro

---
+++ Strings/intro_strings.java
@@ -0,0 +1,11 @@
+public class intro_strings {
+    public static void main(String[] args) {
+        
+        String s1 = new String("World");
+        System.out.println(s1); //this also works actually
+        // Spoiler Alert -> String is a class
+        // Java : "You don't have to create an object just do this"
+        String name = "Hello";
+        System.out.println(name);
+    }
+}
