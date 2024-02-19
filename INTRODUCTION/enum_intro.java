94e90326a97c0e59bcb97fabf3366ed7f576830c
indra-jith <indrajithmp77@gmail.com>
2024-02-19 20:03:51 +0530

    enums

--- INTRODUCTION/enum_intro.java
+++ INTRODUCTION/enum_intro.java
@@ -4,10 +4,40 @@ enum Days {
     WEDNESDAY
 }
 
+enum Levels {
+    EASY,
+    MEDIUM,
+    HARD
+}
+
+class SetDifficulty {
+    Levels level;
+
+    public SetDifficulty(Levels level) {
+        this.level = level;
+    }
+
+    public void SetDiff() {
+        switch (level) {
+            case EASY:
+                System.out.println("Easy Mode");
+                break;
+            case MEDIUM:
+                System.out.println("Medium Mode");
+                break;
+            case HARD:
+                System.out.println("Hard Mode");
+                break;
+        }
+    }
+}
+
 public class enum_intro {
     public static void main(String[] args) {
         for (Days i : Days.values()) {
             System.out.println(i);
         }
+        SetDifficulty s1 = new SetDifficulty(Levels.MEDIUM);
+        s1.SetDiff();
     }
 }
