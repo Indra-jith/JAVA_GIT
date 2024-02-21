1c6c7b1b20e69a7ee04718e10d2f54954dbfa837
indra-jith <indrajithmp77@gmail.com>
2024-02-21 19:34:31 +0530

    anonymous object

--- Oop/anonymousObject.java
+++ Oop/anonymousObject.java
@@ -18,8 +18,12 @@ class Calculation {
 
 public class anonymousObject {
     public static void main(String[] args) {
-        new Calculation(0);
-        // object created without storing location in a reference variable
+        new Calculation(0); // -> Anonymous object
+        // // object created without storing location in a reference variable
+        new Calculation(10).area(); // can be used
+        // BUT we won't be able to do anything like change the area after this
 
+        // Used when you need to create an object ONCE in your code
+        // you can do it this way without creating a reference variable
     }
 }
