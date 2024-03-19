9e014588fc3da01f3966c0b64fd173600af277dd
indra-jith <indrajithmp77@gmail.com>
2024-03-19 20:24:04 +0530

    threads , sleep

--- Oop/threads.java
+++ Oop/threads.java
@@ -4,6 +4,12 @@ class A extends Thread {
         // therefore if using thread, use run method
         for (int i = 0; i <= 20; i++) {
             System.out.println("Executing A");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } // will wait 10 milli second before executing next one
         }
     }
 }
@@ -12,6 +18,12 @@ class B extends Thread {
     public void run() {
         for (int i = 0; i <= 20; i++) {
             System.out.println("From B");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -20,7 +32,19 @@ public class threads {
     public static void main(String[] args) {
         A obj1 = new A();
         B obj2 = new B();
+        System.out.println(obj1.getPriority());
+        System.out.println(obj2.getPriority());
+        // min priority - max priority (1 to 10)
+
+        // to change priority (we can only optimize it, not change the algo)
+        obj1.setPriority(Thread.MAX_PRIORITY);
         obj1.start(); // use start
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         obj2.start(); // same here
         // (without thread keyword) -> not multitasking
     }
