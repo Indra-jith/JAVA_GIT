ecb36007564ec200348be7f2939893fc14add378
indra-jith <indrajithmp77@gmail.com>
2024-04-08 19:29:27 +0530

    set

---
+++ Collections/Set/set_intro.java
@@ -0,0 +1,15 @@
+import java.util.Set;
+import java.util.HashSet;
+
+// Another category -> HashSet , LinkedHashSet, EnumSet, TreeSet
+public class set_intro {
+    public static void main(String[] args) {
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(1);
+        set1.add(2);
+        Set<Integer> set2 = new HashSet<>();
+        HashSet<Integer> set1 = new HashSet(20, 0.5);
+
+        // capacity -> number of elements set1
+    }
+}
