2406e6b433548c1302267c10187275c46d49b57e
indra-jith <indrajithmp77@gmail.com>
2024-04-09 20:51:05 +0530

    date time

---
+++ Date and Time/data_time_intro.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class data_time_intro {
+
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+        date = date.plusDays(1);
+        System.out.println(date);
+        LocalTime time = LocalTime.now();
+        System.out.println(time);
+        LocalDateTime datetime = LocalDateTime.now();
+        System.out.println(datetime);
+        System.out.println("Before formatting : " + datetime);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        System.out.println("After formatting " + datetime.format(formatter));
+    }
+}
