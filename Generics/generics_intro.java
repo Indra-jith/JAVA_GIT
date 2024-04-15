3d960f5703b42aac5025ae0226287d3969d050c8
indra-jith <indrajithmp77@gmail.com>
2024-04-15 20:46:54 +0530

    bounded generic class

--- Generics/generics_intro.java
+++ Generics/generics_intro.java
@@ -16,13 +16,22 @@ class Test {
     }
 }
 
+// What if we want to restrict to certain data types (Example : only Numbers)
+class GenericClass2<T extends Number> {
+    void print() {
+        System.out.println("From Bounded generic class");
+    }
+}
+
 public class generics_intro {
     public static void main(String[] args) {
-        // GenericClass<Integer> obj1 = new GenericClass(5);
-        // System.out.println(obj1.getData());
+        GenericClass<Integer> obj1 = new GenericClass(5);
+        System.out.println(obj1.getData());
 
-        // GenericClass<String> obj2 = new GenericClass("Hello");
-        // System.out.println(obj2.getData());
+        GenericClass<String> obj2 = new GenericClass("Hello");
+        System.out.println(obj2.getData());
+
+        GenericClass2<Integer> obj3 = new GenericClass2();
 
         Test t1 = new Test();
         t1.genericMethod(10);
